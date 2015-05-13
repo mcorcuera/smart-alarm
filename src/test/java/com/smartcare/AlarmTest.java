26d052358908b6cc7261ecedb97b9370679d570d
mcorcuera <mik.corcuera@gmail.com>
2015-05-13 19:08:11 +0200

    Add testing

---
+++ src/test/java/com/smartcare/AlarmTest.java
@@ -0,0 +1,14 @@
+package com.smartcare;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlarmTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
