c97f93e01636ef06a69366cd7b53eb9cddfcb6d8
mcorcuera <mik.corcuera@gmail.com>
2015-05-13 19:51:45 +0200

    New message

--- src/main/java/com/smartcare/Alarm.java
+++ src/main/java/com/smartcare/Alarm.java
@@ -3,7 +3,7 @@ package com.smartcare;
 public class Alarm {
 
 	public static void main(String[] args) {
-		System.out.println( "Wake the fuck up!");
+		System.out.println( "Wake the fuck up !");
 	}
 
 }
